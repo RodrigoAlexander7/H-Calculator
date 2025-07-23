e72e1ebf7edac384da61bde483f939ea7c5e7e07
RodryAlexander <rodrygoleu7@gmail.com>
2025-07-23 15:27:26 -0500

    Adding Picker option

--- IronSuplementCalculator/app/(tabs)/(diagnostic)/diagnostic.tsx
+++ IronSuplementCalculator/app/(tabs)/(diagnostic)/diagnostic.tsx
@@ -1,9 +1,30 @@
-import { Button, Text, View } from 'react-native-ui-lib';
+import { useState } from 'react';
+import { Button, Picker, Text, View } from 'react-native-ui-lib';
 
 export default function DiagnosticScreen() {
+   type ProovinceItem = {
+      label: string;
+      value: number;
+   }
+
+   const items:ProovinceItem[] = [
+      { label:'Opcion 01', value: 1,},
+      { label:'Opcion 02', value: 2,}
+   ]
+
+   const [province,setProvince] = useState()
    return(
       <View useSafeArea>
          <View centerH>
+            <Picker
+               preset='outline'
+               label='Selecciona Provincia'
+               labelColor= 'black'
+               placeholder='Provincia'
+               items = {items}
+               value={province}
+               onChange={setProvince}
+            />
             <Text>Calcular diagnostico</Text>   
             <Button
                label='Calcular'   
