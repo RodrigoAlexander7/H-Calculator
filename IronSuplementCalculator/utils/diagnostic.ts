b883674de5b2345db5cac82b3ec836bbf1120051
RodryAlexander <rodrygoleu7@gmail.com>
2025-07-30 09:16:43 -0500

    Addig constants for dates and male rules

--- IronSuplementCalculator/utils/diagnostic.ts
+++ IronSuplementCalculator/utils/diagnostic.ts
@@ -1,5 +1,9 @@
 import dayjs from 'dayjs';
 
+const WEEK : number = 7
+const MONTH : number = 30
+const YEAR : number = 365
+
 const calculateDiagnostic = (dateBirthStr:string ,gender:string, isGestant:boolean,isPuerper:boolean,gestationTime:string, hbStr:string, hbCorrectionStr:string):string|undefined => {
    const ageDays : number = Number(dayjs().diff(dayjs(dateBirthStr),'day'))
    const hbCorrection:number = Number(hbCorrectionStr)
@@ -7,8 +11,32 @@ const calculateDiagnostic = (dateBirthStr:string ,gender:string, isGestant:boole
 
    hb = hb - hbCorrection
 
-   const rulesGeneric = [
-      {ageMax : 7, stats:[{anemiaLimit:13, result:'Caso de Anemia' }]} ,
+   const genericRules = [
+      {ageMax : WEEK, stats:[{anemiaLimit:13, result:'Caso de Anemia' }]} ,
+      {ageMax : 4*WEEK, stats:[{anemiaLimit:10, result:'Caso de Anemia'}]},
+      {ageMax : 8 * WEEK, stats:[{anemiaLimit:8, result:'Caso de Anemia'}]},
+      {ageMax : 2*MONTH, stats:[{anemiaLimit:13.49,result:'Caso de Anemia'}]}, 
+      {ageMax : 6*MONTH, stats:[{anemiaLimit:9.49,result:'Caso de Anemia'}]}, 
+      {ageMax : 2 * YEAR, stats:[
+         {anemiaLimit:6.99,result: 'Caso de Anemia SEVERA'},
+         {anemiaLimit:9.40,result: 'Caso de Anemia MODERADA'},
+         {anemiaLimit:10.40,result: 'Caso de Anemia LEVE' }
+      ]}, 
+      {ageMax : 5 * YEAR, stats:[
+         {anemiaLimit:6.99,result: 'Caso de Anemia SEVERA'},
+         {anemiaLimit:9.90,result: 'Caso de Anemia MODERADA'},
+         {anemiaLimit:10.90,result: 'Caso de Anemia LEVE' }
+      ]}, 
+      {ageMax : 11 * YEAR, stats:[
+         {anemiaLimit:7.99,result: 'Caso de Anemia SEVERA'},
+         {anemiaLimit:10.90,result: 'Caso de Anemia MODERADA'},
+         {anemiaLimit:11.40,result: 'Caso de Anemia LEVE' }
+      ]}, 
+   ]
+
+   // NOT AJUSTED
+   const maleRules = [
+      {ageMax : , stats:[{anemiaLimit:13, result:'Caso de Anemia' }]} ,
       {ageMax : 28, stats:[{anemiaLimit:10, result:'Caso de Anemia'}]},
       {ageMax : 56, stats:[{anemiaLimit:8, result:'Caso de Anemia'}]},
       {ageMax : 2*30, stats:[{anemiaLimit:13.49,result:'Caso de Anemia'}]}, 
@@ -17,18 +45,9 @@ const calculateDiagnostic = (dateBirthStr:string ,gender:string, isGestant:boole
          {anemiaLimit:6.99,result: 'Caso de Anemia SEVERA'},
          {anemiaLimit:9.40,result: 'Caso de Anemia MODERADA'},
          {anemiaLimit:10.40,result: 'Caso de Anemia LEVE' }
-      ]}, 
-      {ageMax : 60*30, stats:[
-         {anemiaLimit:6.99,result: 'Caso de Anemia SEVERA'},
-         {anemiaLimit:9.90,result: 'Caso de Anemia MODERADA'},
-         {anemiaLimit:10.90,result: 'Caso de Anemia LEVE' }
-      ]}, 
-      {ageMax : 11*12*30, stats:[
-         {anemiaLimit:7.99,result: 'Caso de Anemia SEVERA'},
-         {anemiaLimit:10.90,result: 'Caso de Anemia MODERADA'},
-         {anemiaLimit:11.40,result: 'Caso de Anemia LEVE' }
-      ]}, 
+      ]}
    ]
+
 
    
    if (ageDays < 7)
