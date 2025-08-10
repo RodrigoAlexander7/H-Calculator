4691da8ba5fd7927a5227e33508ce3822fdd25c6
RodryAlexander <rodrygoleu7@gmail.com>
2025-08-10 14:02:47 -0500

    Adding suplement dto

--- IronSuplementCalculator/app/_layout.tsx
+++ IronSuplementCalculator/app/_layout.tsx
@@ -1,11 +1,8 @@
 import { Stack } from "expo-router";
-import { SafeAreaView } from "react-native";
 export default function RootLayout(){
    return(
-      <SafeAreaView>
          <Stack screenOptions={{headerShown:false}}>
             <Stack.Screen name = "(tabs)"/>
          </Stack>
-      </SafeAreaView>
    )
 }
