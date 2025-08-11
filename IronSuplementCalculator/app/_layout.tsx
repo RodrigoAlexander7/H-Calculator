966daf3b65fd45c5d1fcc5aae077fe667285faab
RodryAlexander <rodrygoleu7@gmail.com>
2025-08-11 13:53:42 -0500

    Addig complete UI styles

--- IronSuplementCalculator/app/_layout.tsx
+++ IronSuplementCalculator/app/_layout.tsx
@@ -1,14 +1,20 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
+import { Colors } from 'react-native-ui-lib';
+import "../utils/styles/styles";
+import "../utils/styles/themes";
 
 export default function RootLayout(){
    const colorScheme = useColorScheme();
    const backgroundTheme = colorScheme === 'dark'? DarkTheme : DefaultTheme
+   Colors.setScheme(useColorScheme() === 'dark' ? 'dark' : 'light');
 
    return(
       <ThemeProvider value={backgroundTheme}>
-         <Stack>
+         <Stack screenOptions={{
+            headerShown:false,
+         }}>
             <Stack.Screen name = "(tabs)"/>
          </Stack>
       </ThemeProvider>
---
+++ IronSuplementCalculator/utils/styles/themes.tsx
@@ -0,0 +1,24 @@
+import { Colors } from 'react-native-ui-lib';
+
+Colors.loadSchemes({
+  light: {
+    screenBG: '#F8F9FA',
+    textColor: '#212529',
+    primary: '#2667FF',
+    secondary: '#5C8BFF',
+    accent: '#17A2B8',
+    error: '#DC3545',
+    border: '#ADB5BD',
+    inputBG: '#FFFFFF'
+  },
+  dark: {
+    screenBG: '#121212',
+    textColor: '#E9ECEF',
+    primary: '#5C8BFF',
+    secondary: '#2667FF',
+    accent: '#20C997',
+    error: '#F17C7C',
+    border: '#495057',
+    inputBG: '#1E1E1E'
+  }
+});
